a76bd87154f72ae21e0a178376b25ac6a7fd1663
izadora montenegro <izadoramontenegro@izadoras-MacBook-Pro.local>
2026-01-04 09:37:12 -0300

    update packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  let [list, setList] = useState([]);
+  let [newItem, setNewItem] = useState("")
+
+  useEffect(()=>{
+    setList(["Tarefa1", "Tarefa2"]);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+      <input placeholder="Tarefa" value={newItem} onChange={value => setNewItem(value.target.value)} type="text"/>
+
+      <button className="new-item-button" onClick={() => addNewItem()}>Adicionar nova tarefa</button>
+    <ul className="todo-list">
+      {
+        list.map((item, index) => (
+          <li className="todo-item" key={index}>
+  <span>{item}</span>
+  <button onClick={() => deleteItem(index)}>Deletar</button>
+</li>
+        ))
+      }
+    </ul>
+     </div>
   );
+
+  function addNewItem() {
+    if (newItem.length <= 0) {
+      alert('Por favor, digite algo no campo de texto.');
+      return;
+    }
+
+    let itemIndex = list.indexOf(newItem)
+    if (itemIndex >=0) {
+       alert('Tarefa repetida.');
+      return;
+    }
+
+    setList([...list, newItem]);
+    setNewItem("");
+  }
+
+  function deleteItem(index) {
+    let tempArray = [...list];
+    tempArray.splice(index, 1);
+    setList(tempArray);
+  }
 }
 
 export default App;
